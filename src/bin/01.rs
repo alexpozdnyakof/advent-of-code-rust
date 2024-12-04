8514e4a75a50217521acfa9d6ffde137584fc3c5
Aleksandr Pozdnyakov <alexpozdnyakof@gmail.com>
2024-12-04 19:54:00 +0700

    refactor(01): extract data processing function from first part solution

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -10,7 +10,7 @@ const DAY: &str = "01"; // TODO: Fill the day
 const INPUT_FILE: &str = concatcp!("input/", DAY, ".txt");
 
 const TEST: &str = "\
-<TEST-INPUT>
+3   8
 "; // TODO: Add the test input
 
 fn main() -> Result<()> {
@@ -20,37 +20,33 @@ fn main() -> Result<()> {
     //region Part 1
     println!("=== Part 1 ===");
 
-    fn part1<R: BufRead>(reader: R) -> Result<usize> {
-        // TODO: Solve Part 1 of the puzzle
-        //let answer = reader.lines().flatten().count();
+    fn split_columns<R: BufRead>(reader: R) -> (Vec<i32>, Vec<i32>) {
         let mut left_column = Vec::<i32>::new(); 
         let mut right_column = Vec::<i32>::new(); 
-        let mut result = Vec::<i32>::new(); 
-        let x = reader.lines().flatten().enumerate();
-        for (i, line) in x {
+        let x = reader.lines().flatten();
+        for line in x {
          let split: Vec<i32> = line.split("   ").map(|s| {
             s.parse().unwrap()
          }).collect();
 	 left_column.push(split[0]);
 	 right_column.push(split[1]);
-         println!("{} {:?}", i, split);
         }
 	left_column.sort();
 	right_column.sort();
-	
-	for n in 0..left_column.len() {
-	    result.push((left_column[n] - right_column[n]).abs());
+        (left_column, right_column)
+    }
+
+    fn part1<R: BufRead>(reader: R) -> Result<i32> {
+        let mut result = Vec::<i32>::new();
+	let (left, right) = split_columns(reader);
+	for n in 0..left.len() {
+	    result.push((left[n] - right[n]).abs());
 	}
-	let sum = result.into_iter().reduce(|a, b| a + b); 
-        println!("{:?}", left_column);
-        println!("{:?}", right_column);
-        println!("{:?}", sum);
-    	
-	Ok(1)
+	Ok(result.iter().fold(0, |acc, v| acc + v))
     }
 
     // TODO: Set the expected answer for the test input
-    //assert_eq!(1, part1(BufReader::new(TEST.as_bytes()))?);
+    assert_eq!(5, part1(BufReader::new(TEST.as_bytes()))?);
 
     let input_file = BufReader::new(File::open(INPUT_FILE)?);
     let result = time_snippet!(part1(input_file)?);
