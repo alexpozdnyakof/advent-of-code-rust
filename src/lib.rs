6ec38f78209de09c0daf0de4b8e868eedfdc8c9b
Aleksandr Pozdnyakov <alexpozdnyakof@gmail.com>
2024-12-20 18:36:15 +0700

    feat(lib): add chars slice to string function

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,10 @@ pub fn start_day(day: &str) {
     println!("Advent of Code 2024 - Day {:0>2}", day);
 }
 
+pub fn chars_slice_to_str(slice: &[char]) -> String {
+    slice.into_iter().collect()
+}
+
 // Additional common functions
 
 #[cfg(test)]
@@ -12,4 +16,12 @@ mod tests {
     fn it_works() {
         start_day("00");
     }
+
+    #[test]
+    fn chars_to_slice_test() {
+        let chars = vec!['m', 'u', 'l'];
+        let result = chars_slice_to_str(&chars[0..3]);
+        assert_eq!(result, "mul"); 
+    }
+    
 }
