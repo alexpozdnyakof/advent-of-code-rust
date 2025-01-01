732598bf73d98b36d4df1d0612f96327cf20b343
Aleksandr Pozdnyakov <alexpozdnyakof@gmail.com>
2025-01-01 22:30:36 +0700

    refactor(04): rewrite first part caching

--- src/bin/04.rs
+++ src/bin/04.rs
@@ -1,6 +1,7 @@
 use anyhow::*;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
+use std::collections::HashMap;
 use code_timing_macros::time_snippet;
 use const_format::concatcp;
 use adv_code_2024::*;
@@ -21,59 +22,52 @@ MAMMMXMMMM
 MXMXAXMASX
 ";
 
-fn get_line_char(line: String, n: usize) -> char {
-    line.chars().nth(n).unwrap()
-}
+fn l_char(s: &String) -> char { s.chars().nth(s.len()-1).unwrap() }
 
 fn main() -> Result<()> {
     start_day(DAY);
-    const XMAS: &str = "XMAS";
-    const XMAS_REV: &str = "SAMX";
     //region Part 1
     println!("=== Part 1 ===");
 
     fn part1<R: BufRead>(reader: R) -> Result<usize> {
-        let lines = reader.lines().flatten().collect::<Vec<_>>();
+        let word = "XMAS";
+        let word_rev = word.chars().rev().collect::<String>();
+        let is_tail = |ch: char| { ch == word.chars().nth(0).unwrap() || ch == l_char(&word.to_string()) };
+        
         let mut result: usize = 0;
-        let mut buf_cache: Vec<Vec<char>> = Vec::new();
-        for (i, line) in lines.into_iter().enumerate() {
+        let mut cache: HashMap<(usize,usize), char> = HashMap::new();
+        
+        for (i, line) in reader.lines().flatten().enumerate() {
             let chars = line.chars().collect::<Vec<_>>();
             let mut n: usize = 0;
-            
             while n < chars.len() {
-                if chars[n] == 'X' || chars[n] == 'S'  {
-                    if chars.len() - n > 3 {
-                       let x_str = chars_slice_to_str(&chars[n..n+4]); 
-                       if x_str == XMAS || x_str == XMAS_REV {
-                           result += 1;
-                       }
-                    }
-                    if buf_cache.len() >= XMAS.len() - 1 {
-                       {
-                           let x_str = chars_slice_to_str(vec![chars[n], buf_cache[buf_cache.len()-1][n], buf_cache[buf_cache.len()-2][n], buf_cache[buf_cache.len()-3][n]].as_slice());
-                           if x_str == XMAS || x_str == XMAS_REV {
-                               result += 1;
-                           }
-                       }
-        
-                       if n > 2 {
-                          let x_str = chars_slice_to_str(vec![chars[n], buf_cache[buf_cache.len()-1][n-1], buf_cache[buf_cache.len()-2][n-2], buf_cache[buf_cache.len()-3][n-3]].as_slice());
-                          if x_str == XMAS || x_str == XMAS_REV {
-                              result += 1;
+                 if is_tail(chars[n]) { cache.insert((i,n),chars[n]); };
+
+                 let mut c: usize = 1;
+                 let mut buf = vec![chars[n].to_string(); 4];
+                 while c < word.len() {
+                     if is_tail(chars[n]) && chars.len() - n >= word.len() { buf[0].push(chars[n+c]); }
+                     if c <= i { 
+                          if (buf[1].len() <= 1 || !is_tail(l_char(&buf[1]))) && cache.contains_key(&(i-c,n)) {
+                             buf[1].push(*cache.get(&(i-c,n)).unwrap()); 
                           }
-                       }
-                       if chars.len() - n > 3 {
-                          let x_str = chars_slice_to_str(vec![chars[n], buf_cache[buf_cache.len()-1][n+1], buf_cache[buf_cache.len()-2][n+2], buf_cache[buf_cache.len()-3][n+3]].as_slice());
-                          if x_str == XMAS || x_str == XMAS_REV {
-                             result += 1;
-                          }     
-                       }  	
-                    } 
-                }
-                n += 1;   
-            } 
-            
-            buf_cache.push(chars);
+                          if c <= n && (buf[2].len() <= 1 || !is_tail(l_char(&buf[2]))) && cache.contains_key(&(i-c,n-c)){
+                             buf[2].push(*cache.get(&(i-c,n-c)).unwrap()); 
+                         }
+                         if c <= chars.len() - n && (buf[3].len() <= 1 || !is_tail(l_char(&buf[3]))) && cache.contains_key(&(i-c,n+c)) {
+                             buf[3].push(*cache.get(&(i-c,n+c)).unwrap()); 
+                         }
+                     }
+                      
+                     c += 1;
+                 }
+                 for (si, s) in buf.iter().map(|buf_str| { buf_str.chars().rev().collect::<String>() }).enumerate() {
+                     if s == &word[0..s.len()] || s == &word_rev[0..s.len()]  {
+                         if s.len() == word.len() { result +=1; } else if si > 0 { cache.insert((i,n),chars[n]); }
+                     }  
+                 }
+                 n +=1;
+            }
             
         }
         Ok(result)
