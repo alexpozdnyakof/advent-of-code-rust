46514540458996b39ad4baca5b993dbf8f6ec7fb
Aleksandr Pozdnyakov <alexpozdnyakof@gmail.com>
2025-04-14 15:32:50 +0700

    feat(05): add first part solution

--- src/bin/05.rs
+++ src/bin/05.rs
@@ -4,6 +4,8 @@ use std::io::{BufRead, BufReader};
 use code_timing_macros::time_snippet;
 use const_format::concatcp;
 use adv_code_2024::*;
+use std::collections::HashMap;
+use std::convert::TryInto;
 
 const DAY: &str = "05"; // TODO: Fill the day
 const INPUT_FILE: &str = concatcp!("input/", DAY, ".txt");
@@ -37,7 +39,51 @@ const TEST: &str = "\
 75,97,47,61,53
 61,13,29
 97,13,75,29,47
-"; // TODO: Add the test input
+"; 
+
+fn collect_array<T, I, const N: usize>(itr: I) -> [T; N]
+where
+    T: Default + Copy,
+    I: IntoIterator<Item = T>,
+{
+    let mut res = [T::default(); N];
+    for (it, elem) in res.iter_mut().zip(itr) {
+        *it = elem
+    }
+
+    res
+}
+
+struct Graph {
+    data: HashMap<usize, Vec<usize>>,
+}
+
+impl Graph {
+    fn new() -> Graph {
+        Graph {
+            data: HashMap::new()
+        }
+    }
+    fn add_edge(&mut self, v: usize, e: usize) {
+        if self.data.contains_key(&v) {
+            self.data.get_mut(&v).unwrap().push(e);
+        } else { 
+            self.data.insert(v, vec![e]); 
+        }
+    }
+    
+    fn validate_path(&self, path: &Vec<usize>) -> bool {
+         for (i, edge) in path.into_iter().enumerate() {
+              if i == path.len() - 1 { continue; }
+              if !(self.data.contains_key(&edge) 
+                  && self.data.get(&edge).unwrap().contains(&path[i+1])
+                  ) { return false }
+         }
+         return true
+    }
+
+
+}
 
 fn main() -> Result<()> {
     start_day(DAY);
@@ -46,12 +92,25 @@ fn main() -> Result<()> {
     println!("=== Part 1 ===");
 
     fn part1<R: BufRead>(reader: R) -> Result<usize> {
-        // TODO: Solve Part 1 of the puzzle
-        let answer = reader.lines().flatten().count();
-        Ok(answer)
+        let mut is_parsing_finished = false;
+        let mut graph = Graph::new(); 
+        let mut result: usize = 0;
+        for line in reader.lines().flatten() {
+             println!("{}", line.len());
+             if line.len() == 0 { is_parsing_finished = true; continue; }
+             if !is_parsing_finished {
+                  let [v, e]:[usize; 2] = collect_array(line.split("|").map(|s| { s.parse::<usize>().unwrap()}));
+                  graph.add_edge(v, e);
+             } else {
+                  let path:Vec<usize> = line.split(",").map(|s| { s.parse::<usize>().unwrap() }).collect(); 
+                  if graph.validate_path(&path) {
+                      result += path[(path.len() - 1) / 2]
+                  }
+             }
+        }
+        Ok(result)
     }
 
-    // TODO: Set the expected answer for the test input
     assert_eq!(143, part1(BufReader::new(TEST.as_bytes()))?);
 
     let input_file = BufReader::new(File::open(INPUT_FILE)?);
