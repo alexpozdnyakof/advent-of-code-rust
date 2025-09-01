43cff893bb93cf081cfdc8cbc3519bf82f8d31b3
Aleksandr Pozdnyakov <alexpozdnyakof@gmail.com>
2025-09-01 20:53:22 +0700

    feat(08): add second part solution

--- src/bin/08.rs
+++ src/bin/08.rs
@@ -24,7 +24,7 @@ const TEST: &str = "\
 ............
 "; 
 
-#[derive(Debug, Eq, Hash)]
+#[derive(Debug, Eq, Hash, Copy, Clone)]
 struct Point {
     x: isize,
     y: isize,
@@ -86,23 +86,68 @@ fn main() -> Result<()> {
 
     assert_eq!(14, part1(BufReader::new(TEST.as_bytes()))?);
 
-   let input_file = BufReader::new(File::open(INPUT_FILE)?);
-   let result = time_snippet!(part1(input_file)?);
-   println!("Result = {}", result);
+    let input_file = BufReader::new(File::open(INPUT_FILE)?);
+    let result = time_snippet!(part1(input_file)?);
+    println!("Result = {}", result);
     //endregion
 
     //region Part 2
-    // println!("\n=== Part 2 ===");
-    //
-    // fn part2<R: BufRead>(reader: R) -> Result<usize> {
-    //     Ok(0)
-    // }
-    //
-    // assert_eq!(0, part2(BufReader::new(TEST.as_bytes()))?);
-    //
-    // let input_file = BufReader::new(File::open(INPUT_FILE)?);
-    // let result = time_snippet!(part2(input_file)?);
-    // println!("Result = {}", result);
+    println!("\n=== Part 2 ===");
+    
+    fn part2<R: BufRead>(reader: R) -> Result<usize> {
+        // на каждые координаты запускать поиск до конца поля
+        // добавлять спаренные антенны 
+        let mut broadcasters: HashMap<char, Vec<Point>> = HashMap::new();
+        let mut antinodes: HashSet<Point> = HashSet::new();
+        for (y, line) in reader.lines().flatten().enumerate() {
+            for (x, tag) in line.chars().enumerate() {
+                if tag == '.' || tag == '#' { continue }
+                
+                let antenna_point = Point { x: x as isize, y: y as isize };
+                
+                if let Some(broadcast_list) = broadcasters.get_mut(&tag) {
+                    antinodes.insert(antenna_point);
+
+                    for prev_antenna_point in broadcast_list.iter() {    
+                        antinodes.insert(*prev_antenna_point);
+                        
+                        let mut antinode = get_up_antinode_position(&antenna_point, prev_antenna_point);
+                        let mut prev_antinode = *prev_antenna_point;
+                
+                        while in_field_range(&antinode, line.len()) { 
+                            antinodes.insert(antinode);
+                            let new_antinode = get_up_antinode_position(&prev_antinode, &antinode);
+                            
+                            prev_antinode = antinode;
+                            antinode = new_antinode;
+                        }
+
+                        antinode = get_bottom_antinode_position(&antenna_point, prev_antenna_point);
+                        prev_antinode = antenna_point; 
+                        
+                        while in_field_range(&antinode, line.len()) { 
+                            antinodes.insert(antinode);
+                            let new_antinode = get_bottom_antinode_position(&antinode, &prev_antinode);
+                            
+                            prev_antinode = antinode;
+                            antinode = new_antinode;
+                        }
+                    }
+
+                    broadcast_list.push(antenna_point);
+
+                } else { broadcasters.insert(tag, vec![antenna_point] ); }
+                
+            }
+        }
+        Ok(antinodes.len())
+    }
+    
+    assert_eq!(34, part2(BufReader::new(TEST.as_bytes()))?);
+    
+    let input_file = BufReader::new(File::open(INPUT_FILE)?);
+    let result = time_snippet!(part2(input_file)?);
+    println!("Result = {}", result);
     //endregion
 
     Ok(())
