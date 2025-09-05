9dc3ea816cfd192cecb3bd07d81ffb97e15a06d0
Aleksandr Pozdnyakov <alexpozdnyakof@gmail.com>
2025-09-05 18:37:53 +0700

    refactor(09): extract fragmentation by char fn

--- src/bin/09.rs
+++ src/bin/09.rs
@@ -2,10 +2,10 @@ use adv_code_2024::*;
 use anyhow::*;
 use code_timing_macros::time_snippet;
 use const_format::concatcp;
+use std::collections::BTreeMap;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
-use std::collections::{ BTreeMap } ;
-const DAY: &str = "09"; 
+const DAY: &str = "09";
 const INPUT_FILE: &str = concatcp!("input/", DAY, ".txt");
 
 const TEST: &str = "\
@@ -23,13 +23,13 @@ struct LocationDescriptor {
 type Position = usize;
 
 struct LocationIndex {
-    index: BTreeMap<Position, LocationDescriptor> 
+    index: BTreeMap<Position, LocationDescriptor>,
 }
 
 impl LocationIndex {
     fn new() -> Self {
         LocationIndex {
-            index: BTreeMap::new() 
+            index: BTreeMap::new(),
         }
     }
 
@@ -38,7 +38,7 @@ impl LocationIndex {
         let mut new_size = descriptor.size;
 
         if let Some((&prev_position, prev)) = self.index.range(..=position).last() {
-            if prev_position+prev.size == position && prev.id == descriptor.id {
+            if prev_position + prev.size == position && prev.id == descriptor.id {
                 new_start = prev_position;
                 new_size += prev.size;
                 self.index.remove(&prev_position);
@@ -46,14 +46,19 @@ impl LocationIndex {
         }
 
         if let Some((&next_position, next)) = self.index.range(position..).next() {
-            if new_start + new_size == next_position  && next.id == descriptor.id {
+            if new_start + new_size == next_position && next.id == descriptor.id {
                 new_size += next.size;
                 self.index.remove(&next_position);
             }
-            
         }
 
-        self.index.insert(new_start, LocationDescriptor { id: descriptor.id, size: new_size });
+        self.index.insert(
+            new_start,
+            LocationDescriptor {
+                id: descriptor.id,
+                size: new_size,
+            },
+        );
     }
 
     fn log(&self) {
@@ -61,25 +66,15 @@ impl LocationIndex {
             println!("{:?}: {:?}\n", k, v);
         }
     }
-
 }
 
-
-
 fn create_free_desc(size: usize) -> LocationDescriptor {
-    LocationDescriptor {
-        id: None,
-        size
-    }
+    LocationDescriptor { id: None, size }
 }
 
 fn create_data_desc(size: usize, id: usize) -> LocationDescriptor {
-    LocationDescriptor {
-        id: Some(id),
-        size
-    }
+    LocationDescriptor { id: Some(id), size }
 }
-
 
 fn from_raw(raw: &RawBlocks) -> NTFS {
     let mut ntfs = NTFS::new();
@@ -87,13 +82,16 @@ fn from_raw(raw: &RawBlocks) -> NTFS {
     for (i, ch) in raw.chars().enumerate() {
         if let Some(size) = ch.to_digit(10).map(|n| n as usize) {
             if i % 2 == 0 {
-                ntfs.files.index.insert(offset, create_data_desc(size, i / 2));
+                ntfs.files
+                    .index
+                    .insert(offset, create_data_desc(size, i / 2));
             } else {
-                ntfs.free_space.index.insert(offset, create_free_desc(size) ); 
+                ntfs.free_space.index.insert(offset, create_free_desc(size));
             }
             offset += size;
-            
-        } else { continue; }
+        } else {
+            continue;
+        }
     }
 
     ntfs
@@ -102,88 +100,99 @@ fn from_raw(raw: &RawBlocks) -> NTFS {
 struct NTFS {
     files: LocationIndex,
     free_space: LocationIndex,
-
 }
 
 impl NTFS {
-    
     fn new() -> Self {
         NTFS {
             files: LocationIndex::new(),
-            free_space: LocationIndex::new()
+            free_space: LocationIndex::new(),
         }
     }
 
-
     fn free(&mut self, target: usize, size: usize) -> Result<(usize, usize)> {
         if let Some((&file_position, file)) = self.files.index.range(..=&target).last() {
-            //self.files.index.range(..=target).last() { 
-            if size == file.size{
-                self.free_space.insert(file_position, create_free_desc(file.size));
+            //self.files.index.range(..=target).last() {
+            if size == file.size {
+                self.free_space
+                    .insert(file_position, create_free_desc(file.size));
                 self.files.index.remove(&file_position);
             } else {
                 if let Some(file_id) = file.id {
                     let new_file = create_data_desc(file.size - size, file_id);
                     let free_space = create_free_desc(size);
-                    
-                    self.free_space.insert(file_position + new_file.size, free_space);
+
+                    self.free_space
+                        .insert(file_position + new_file.size, free_space);
 
                     self.files.insert(file_position, new_file);
                 }
-            
             }
             Ok((file_position, size))
-
         } else {
             Err(anyhow!("File: {} not found", target))
-        
         }
     }
 
     fn copy(&mut self, file_position: usize, size: usize, allocated_position: usize) {
-        if let Some(file) = self.files.index.get(&file_position) { 
+        if let Some(file) = self.files.index.get(&file_position) {
             if file.size == size {
                 self.files.insert(allocated_position, *file);
             } else {
                 if let Some(file_id) = file.id {
-                    self.files.insert(allocated_position,  create_data_desc(size, file_id));
+                    self.files
+                        .insert(allocated_position, create_data_desc(size, file_id));
                 }
             }
         }
     }
-    
-    fn check_sum(&self) -> usize {
-        self.files.index.iter().fold(0, |mut acc, (&position, &file)| { 
-            for i in 0..file.size {
-                if let Some(file_id) = file.id {
-                    acc += (position + i) * file_id;
-                }
-            }
-            acc
 
-        })
+    fn check_sum(&self) -> usize {
+        self.files
+            .index
+            .iter()
+            .fold(0, |mut acc, (&position, &file)| {
+                for i in 0..file.size {
+                    if let Some(file_id) = file.id {
+                        acc += (position + i) * file_id;
+                    }
+                }
+                acc
+            })
     }
 
-
-
     fn allocate_before(&mut self, max_position: &usize, size: usize) -> Option<usize> {
-            for (&position, &free) in self.free_space.index.range(..=max_position) {
-                if free.size >= size { 
-                    self.free_space.index.remove(&position);
-                    if free.size > size { self.free_space.insert(&position + size, create_free_desc(free.size - size)); }
-                    return Some(position)
+        for (&position, &free) in self.free_space.index.range(..=max_position) {
+            if free.size >= size {
+                self.free_space.index.remove(&position);
+                if free.size > size {
+                    self.free_space
+                        .insert(&position + size, create_free_desc(free.size - size));
                 }
+                return Some(position);
             }
+        }
 
-            None
+        None
     }
 
     fn print(&self) {
-       let mut e_index = self.files.index.clone();
+        let mut e_index = self.files.index.clone();
         e_index.extend(self.free_space.index.clone());
-        let chars: String = e_index.iter().flat_map(|(_,desc)| { vec![if let Some(id) = desc.id { id.to_string() } else { String::from('.') }; desc.size]}).collect();
+        let chars: String = e_index
+            .iter()
+            .flat_map(|(_, desc)| {
+                vec![
+                    if let Some(id) = desc.id {
+                        id.to_string()
+                    } else {
+                        String::from('.')
+                    };
+                    desc.size
+                ]
+            })
+            .collect();
         println!("{}", chars);
-
     }
 }
 
@@ -194,25 +203,44 @@ fn main() -> Result<()> {
     println!("=== Part 1 ===");
 
     fn part1<R: BufRead>(reader: R) -> Result<usize> {
-    
         if let Some(raw_blocks) = reader.lines().next().transpose()? {
             let mut file_system = from_raw(&raw_blocks);
-            let files: Vec<(usize, LocationDescriptor)> = file_system.files.index.iter().map(|(&k,&v)| (k, v)).collect();
+            let files: Vec<(usize, LocationDescriptor)> = file_system
+                .files
+                .index
+                .iter()
+                .map(|(&k, &v)| (k, v))
+                .collect();
             let chunk_size = 1;
-           'outer: for (position, file) in files.iter().rev() {
+
+            let fragmentate = |position: &usize,
+                               file: &LocationDescriptor,
+                               file_system: &mut NTFS|
+             -> Result<()> {
                 let mut size = file.size;
                 while size != 0 {
-                    if let Some(allocated_position) = file_system.allocate_before(position, chunk_size) {
+                    if let Some(allocated_position) =
+                        file_system.allocate_before(position, chunk_size)
+                    {
                         file_system.copy(*position, chunk_size, allocated_position);
-                        if file_system.free(*position, chunk_size).is_ok() {
+                        let cleared = file_system.free(*position, chunk_size);
+                        if cleared.is_ok() {
                             size -= chunk_size;
                         } else {
-                            println!("Memory not allocated");
-                            break 'outer;
+                            return Err(anyhow!("blabla"));
                         }
-                        
-                    } else { break 'outer; }
-                } 
+                    } else {
+                        return Err(anyhow!("Unable allocate free space"));
+                    }
+                }
+                Ok(())
+            };
+
+            for (position, file) in files.iter().rev() {
+                if let Err(reason) = fragmentate(&position, file, &mut file_system) {
+                    println!("{:?}", reason);
+                    break;
+                }
             }
             file_system.print();
             Ok(file_system.check_sum())
